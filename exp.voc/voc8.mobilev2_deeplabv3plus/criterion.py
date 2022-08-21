7c1ae58499b0adad17662358ebcd5471681c3130
qinjie <qinjie.01@bytedance.com>
2022-08-21 21:56:16 +0800

    add file

---
+++ exp.voc/voc8.mobilev2_deeplabv3plus/criterion.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+import torch
+import numpy as np
+from torch.nn import functional as F
+from torch.autograd import Variable
+import scipy.ndimage as nd
+
+def L2(f_):
+    return (((f_**2).sum(dim=1))**0.5).reshape(f_.shape[0],1,f_.shape[2],f_.shape[3]) + 1e-8
+
+def similarity(feat):
+    feat = feat.float()
+    tmp = L2(feat).detach()
+    feat = feat/tmp
+    feat = feat.reshape(feat.shape[0],feat.shape[1],-1)
+    return torch.einsum('icm,icn->imn', [feat, feat])
+
+def sim_dis_compute(f_S, f_T):
+    sim_err = ((similarity(f_T) - similarity(f_S))**2)/((f_T.shape[-1]*f_T.shape[-2])**2)/f_T.shape[0]
+    sim_dis = sim_err.sum()
+    return sim_dis
+
+
+class CriterionCosineSimilarity(nn.Module):
+    def __init__(self):
+        super(CriterionCosineSimilarity, self).__init__()
+        self.ep = 1e-6
+
+    def forward(self, p, q):
+        sim_matrix = p.transpose(-2, -1).matmul(q)
+        a = torch.norm(p, p=2, dim=-2)
+        b = torch.norm(q, p=2, dim=-2)
+        sim_matrix /= a.unsqueeze(-1)
+        sim_matrix /= b.unsqueeze(-2)
+        return sim_matrix
+
